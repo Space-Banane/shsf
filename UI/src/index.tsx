32715a0855c773c80af22672d8f1306da6de9ace
Space <space@reversed.dev>
2025-04-06 16:56:46 +0200

    Switch to CLI version of tailwind bc WHAT THE FUCK IS THIS BULLSHIT POSTCSS AHHH

--- UI/src/index.tsx
+++ UI/src/index.tsx
@@ -1,8 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"; // Import Tailwind CSS
 import App from "./App";
-import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(container);
@@ -11,9 +8,4 @@ root.render(
 	<App />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-export const BASE_URL = "http://localhost:5000"
+export const BASE_URL = process.env.UI_URL;
