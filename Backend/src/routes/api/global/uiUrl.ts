dfeac2b4f749105502967380c5d3226ecf10b23a
Space-Banane <paulwaehner923@gmail.com>
2026-03-18 17:39:12 +0100

    feat: Update UI URL endpoint to remove authentication requirements and simplify documentation

--- Backend/src/routes/api/global/uiUrl.ts
+++ Backend/src/routes/api/global/uiUrl.ts
@@ -8,25 +8,9 @@ export = new fileRouter.Path("/").http("GET", "/api/global/uiUrl", (http) =>
 		.ratelimit((limit) => limit.hits(20).window(5000).penalty(100))
 		.document({
 			description:
-				"Returns the UI URL of this SHSF instance. Called by SHSF.dev to display the instance UI URL. Authenticated via instance secret and linked user email.",
+				"Returns the UI URL of this SHSF instance. (usually) called by SHSF.dev to display the instance UI URL. Not Authenticated as it can be easily guessed anyway.",
 			tags: ["Global"] as OpenAPITags[],
 			operationId: "getGlobalUIUrl",
-			parameters: [
-				{
-					name: "x-shsf-insect",
-					in: "header",
-					required: true,
-					description: "The instance secret.",
-					schema: { type: "string" },
-				},
-				{
-					name: "x-shsf-address",
-					in: "header",
-					required: true,
-					description: "The linked user email address.",
-					schema: { type: "string" },
-				},
-			],
 			responses: {
 				200: {
 					description: "Returns the version of this SHSF instance.",
@@ -45,25 +29,6 @@ export = new fileRouter.Path("/").http("GET", "/api/global/uiUrl", (http) =>
 			},
 		})
 		.onRequest(async (ctr) => {
-			const secret = ctr.headers.get("x-shsf-insect");
-			const email = ctr.headers.get("x-shsf-address");
-
-			const authCheck = await GlobalAuthCheck(secret);
-
-			if (!authCheck.success) {
-				return ctr.status(ctr.$status.UNAUTHORIZED).print({
-					status: 401,
-					message: authCheck.message,
-				});
-			}
-
-			if (!email || email !== authCheck.linked_email) {
-				return ctr.status(ctr.$status.UNAUTHORIZED).print({
-					status: 401,
-					message: "Invalid or mismatched address.",
-				});
-			}
-
 			return ctr.print({
 				status: "OK",
 				uiUrl: env.UI_URL,
